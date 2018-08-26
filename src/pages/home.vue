54c4c6068d95c2a7f581917d6e6e211fc8261f20
alemon <1215784933@qq.com>
2018-08-27 02:10:25 +0800

    test3

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -2,7 +2,7 @@
     <div>
         <el-container>
             <el-main>
-                <h1>test merge2</h1>
+                <h1>test merge3</h1>
             </el-main>
         </el-container>
     </div>
